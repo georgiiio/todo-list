7bcb7d525d2bb6f75d8a4dd054da8c161137ee05
Georgiiio <gosha.pronkin@mail.ru>
2022-11-07 19:23:08 +0100

    input validation

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const todoList = [
   }, 
 ]
 
+let taskElement, buttonElement, intputTitle, todoForm
+
 function renderTodoItem (parentElement,todoItem){
   parentElement.innerHTML = parentElement.innerHTML + `<li class="item ${todoItem.isCompeleted ? 'completed' : ''}">
   <p>${todoItem.title}</p>
@@ -21,32 +23,47 @@ function renderTodoItem (parentElement,todoItem){
 }
 
 function onSubmit(event){
-  console.log(event);
   event.preventDefault();
   const submittedForm = event.target
   const input = submittedForm.title
-  console.log(input.value)
-
   todoList.push({
     title: input.value,
     isCompeleted: false,
   })
   renderTodoList();
+  input.value = '';
+  buttonElement.setAttribute('disabled', '');
 }
 
 function renderTodoList (){
-  const taskElement = document.querySelector('#tasks');
   taskElement.innerHTML = '';
   todoList.forEach(function(todoListItem){
     renderTodoItem(taskElement, todoListItem);
   })
 }
 
-function onAppReady(){
-  const todoForm = document.querySelector('#todo-form'); 
+function onInput(event) {
+  const value = event.target.value
+  if (value === ''){
+    buttonElement.setAttribute('disabled', '');
+  } else {
+    buttonElement.removeAttribute('disabled');
+  }
+}
+
+function onAppReady() {
+  intputTitle = document.querySelector('#task-title');
+  todoForm = document.querySelector('#todo-form');
+  taskElement = document.querySelector('#tasks');
+  buttonElement = document.querySelector('#add-btn');
+
+  intputTitle.addEventListener('input', onInput); 
   todoForm.addEventListener('submit', onSubmit);
+
   renderTodoList();
 }
+
+
 
 document.addEventListener('DOMContentLoaded', onAppReady);
 
